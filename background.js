b14e27cc2f748f37311af55aad39f0ec33020704
linfeng <xz874583556@foxmail.com>
2020-02-14 12:51:47 +0800

    change config

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+function createWindow() {
     // 创建浏览器窗口
     const win = new BrowserWindow({
         width: 800,
@@ -8,36 +8,36 @@ function createWindow () {
         webPreferences: {
             nodeIntegration: true
         }
-    })
+    });
 
     // and load the index.html of the app.
-    win.loadFile('./dist/index.html')
+    win.loadFile('./dist/index.html');
 
     // 打开开发者工具
-    win.webContents.openDevTools()
+    win.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // 部分 API 在 ready 事件触发后才能使用。
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
     // 否则绝大部分应用及其菜单栏会保持激活。
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', () => {
     // 在macOS上，当单击dock图标并且没有其他窗口打开时，
     // 通常在应用程序中重新创建一个窗口。
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        createWindow();
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. 也可以拆分成几个文件，然后用 require 导入。
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-module.exports={
+module.exports = {
+    // vue-cli3之后需要配置这个来控制图标
+    pwa: {
+        iconPaths: {
+            favicon32: 'favicon.ico',
+            favicon16: 'favicon.ico',
+            appleTouchIcon: 'favicon.ico',
+            maskIcon: 'favicon.ico',
+            msTileImage: 'favicon.ico'
+        }
+    },
+    // 不配置这个路径会有dist文件的html打开是空白的问题
     runtimeCompiler: true,
     publicPath: './'
-}
+};
